b4d399556ec3743edb0c8f90b9df7f71b510ada1
diewoom <diego@kambista.com>
2020-03-11 00:06:58 -0500

    first commit

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PlaceList from '../components/PlaceList'
+import {useParams} from 'react-router-dom'
+const DUMMY_PLACES =
+[
+ {
+   id:'p1',
+   title:'Empire State Buiding',
+   description: 'One of the most famous sky scrapers in the world',
+   imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Empire_State_Building_by_David_Shankbone_crop.jpg/220px-Empire_State_Building_by_David_Shankbone_crop.jpg',
+   address: '20 W 34th ST, New York , NY 1001',
+   coordinates:{
+     lat: 40.747474,
+     lng: -74.782222
+  },
+  creatorId: 'u1'
+ },
+   {
+       id:'p2',
+       title:'Empire State Buiding',
+       description: 'One of the most famous sky scrapers in the world',
+       imageUrl: 'https://media.tacdn.com/media/attractions-splice-spp-674x446/06/74/aa/fc.jpg',
+       address: '20 W 34th ST, New York , NY 1001',
+       coordinates:{
+         lat: 40.747474,
+         lng: -74.782222
+     },
+     creatorId:'u2'
+   }  
+ ]
+const UserPlaces = () => {
+    const userId = useParams().userId
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creatorId === userId)
+    return (
+      <PlaceList items={loadedPlaces}/>
+    )
+}
+
+export default UserPlaces;
---
+++ src/user/pages/Users.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import UserList from '../components/UsersList'
+const Users = () => {
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Diego',
+      image: 
+        'https://kinsta.com/es/wp-content/uploads/sites/8/2017/04/cambiar-wordpress-url-1.png',
+      places: 3
+    }
+  ]
+
+
+  return <UserList items={USERS}/>
+}
+
+export default Users;
